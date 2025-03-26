7a73dc0749586dee4e52558b9e6467bada1c1110
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-26 17:20:53 +0800

    Prog10: Pseudocode Added

---
+++ practice_python_program_batch_05/prog10_convert_name_snake_case.py
@@ -0,0 +1,10 @@
+#Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
+#Example:
+#Input: jUAn DEla CrUZ
+#Output: juan_dela_cruz
+
+#Ask User for name
+
+#Convert name to snake case
+
+#print name
