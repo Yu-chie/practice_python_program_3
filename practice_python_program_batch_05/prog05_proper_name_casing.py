555cc9f871d4c98b66802f71ba25e4597cbcc1c8
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-26 17:20:09 +0800

    Prog05: Pseudocode Added

---
+++ practice_python_program_batch_05/prog05_proper_name_casing.py
@@ -0,0 +1,10 @@
+#Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+#Example:
+#Input: jUAn DEla CrUZ
+#Output: Juan Dela Cruz
+
+#Ask User for name
+
+#Correct name casing
+
+#Print name
