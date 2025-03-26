f6836e01d8e6fc715b0b73821828884625ca20d6
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-26 17:20:34 +0800

    Prog08: Pseudocode Added

---
+++ practice_python_program_batch_05/prog08_count_characters_in_name.py
@@ -0,0 +1,10 @@
+#Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+#Example:
+#Input: Juan Dela Cruz
+#Output: 14
+
+#Ask User for name
+
+#Count characters in name
+
+#print number of characters
