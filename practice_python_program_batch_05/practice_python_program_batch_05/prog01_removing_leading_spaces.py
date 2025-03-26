37508af127ef091e769eb93cb43529652f8c1c4f
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-26 16:56:06 +0800

    Prog01: Pseudocode Added

---
+++ practice_python_program_batch_05/practice_python_program_batch_05/prog01_removing_leading_spaces.py
@@ -0,0 +1,10 @@
+#Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+#Example:
+    #Input:         Juan Dela Cruz
+    #Output: Juan Dela Cruz
+
+#Ask User to input their Name
+
+#Remove leading spaces
+
+#Print Name
