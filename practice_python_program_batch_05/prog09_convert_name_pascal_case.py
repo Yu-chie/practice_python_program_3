27f4a9d5cf0f121b1ea5a4f2b192093d9e71b100
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-26 17:20:43 +0800

    Prog09: Pseudocode Added

---
+++ practice_python_program_batch_05/prog09_convert_name_pascal_case.py
@@ -0,0 +1,10 @@
+#Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
+#Example:
+#Input: jUAn DEla CrUZ
+#Output: JuanDelaCruz
+
+#Ask User for name
+
+#Convert name to pascal case
+
+#Print name
