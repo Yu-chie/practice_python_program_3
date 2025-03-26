49f66c636983b0170e1b2376d300e96b5e759a74
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-26 17:45:51 +0800

    Prog01: Pseudocode Added

---
+++ practice_python_program_batch_06/prog01_removing_leading_spaces_without_lstrip.py
@@ -0,0 +1,7 @@
+#Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+
+#Ask the user to input a string with leading spaces.
+
+#Find index of first non-space characters
+
+#Extract and print clean string()
