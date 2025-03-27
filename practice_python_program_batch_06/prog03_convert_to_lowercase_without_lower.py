b5bcbfd3bbdad6eb8b8a52f248fbba600ce37d7b
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-27 12:39:46 +0800

    Prog03: Pseudocode Added

---
+++ practice_python_program_batch_06/prog03_convert_to_lowercase_without_lower.py
@@ -0,0 +1,7 @@
+#Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+
+#Ask User for text
+
+#Convert uppercase to lowercase
+
+#Print text
