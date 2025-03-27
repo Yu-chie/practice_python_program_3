289cb76a91d51e7b7a1e998271b9d3a0fe761a93
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-27 12:31:35 +0800

    Prog02: Pseudocode Added

---
+++ practice_python_program_batch_06/prog02_remove_prefix_without_removeprefix.py
@@ -0,0 +1,9 @@
+#Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
+
+#Ask User for text and a prefix to remove(
+ 
+#Check if text starts with the prefix()
+
+#Remove prefix
+
+#Print modified text
