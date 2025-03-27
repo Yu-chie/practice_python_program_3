10c995e1a8762184ac4335b8b2e54e0c36505a12
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-27 12:32:50 +0800

    Prog02: Ask User for text and prefix to remove

--- practice_python_program_batch_06/prog02_remove_prefix_without_removeprefix.py
+++ practice_python_program_batch_06/prog02_remove_prefix_without_removeprefix.py
@@ -1,7 +1,9 @@
 #Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
 
-#Ask User for text and a prefix to remove(
- 
+#Ask User for text and a prefix to remove
+text = input("Enter a text: ")
+prefix = input("Enter prefix to remove: ")
+
 #Check if text starts with the prefix()
 
 #Remove prefix
