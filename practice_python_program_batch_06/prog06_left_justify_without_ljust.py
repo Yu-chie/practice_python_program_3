0f56adef0450b17b3b1ce07b71ee849300a89708
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-28 18:47:59 +0800

    Prog06: Pseudocode Added

---
+++ practice_python_program_batch_06/prog06_left_justify_without_ljust.py
@@ -0,0 +1,8 @@
+#Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter.
+#Create a program that do the same functionality without using ljust() function.
+
+#Ask user for text and total length to justify
+
+#Calculate and append number of spaces to reach the total length.
+
+#Print the result
