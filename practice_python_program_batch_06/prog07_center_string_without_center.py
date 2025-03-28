7eb28684c56f61e079ebdecf332aed6c7558d9ff
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-28 19:10:02 +0800

    Prog07: Pseudocode Added

---
+++ practice_python_program_batch_06/prog07_center_string_without_center.py
@@ -0,0 +1,8 @@
+#Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. 
+#Create a program that do the same functionality without using center() function.
+
+#Ask user to enter a text and length to center
+
+#Calculate spaced and center text
+
+#Print centered string
