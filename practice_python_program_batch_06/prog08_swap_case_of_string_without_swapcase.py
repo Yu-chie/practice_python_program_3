39b6909d4a9ed6d41efe7ac5bcda42b67300548c
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-28 19:15:45 +0800

    Prog08: Pseudocode Added

---
+++ practice_python_program_batch_06/prog08_swap_case_of_string_without_swapcase.py
@@ -0,0 +1,8 @@
+#Prog08. swapcase() reverse the casing of each of the character of the string. 
+#Create a program that do the same functionality without using swapcase() function.
+
+#Ask User to enter text
+
+#Loop through each character and swap its case
+
+#Print swapped case text
