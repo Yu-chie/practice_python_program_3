81d8daac38cf7697af188751a924b0041509c609
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-28 18:26:10 +0800

    Prog04: Pseudocode Added

---
+++ practice_python_program_batch_06/prog04_check_if_uppercase_without_isupper.py
@@ -0,0 +1,8 @@
+#Prog04. isupper() check if all characters of the string is on upper case.
+#Create a program that do the same functionality without using isupper() function.
+
+#Ask User for text
+
+#Create a loop to iterate through each character
+
+#Print the result
