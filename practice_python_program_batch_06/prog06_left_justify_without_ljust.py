edfa7a4dc4c96e111279e54c3c89e598fc7177fe
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-28 18:49:05 +0800

    Prog06: Ask ser for text and total length to justify

--- practice_python_program_batch_06/prog06_left_justify_without_ljust.py
+++ practice_python_program_batch_06/prog06_left_justify_without_ljust.py
@@ -2,6 +2,8 @@
 #Create a program that do the same functionality without using ljust() function.
 
 #Ask user for text and total length to justify
+text = input("Enter a Text: ")
+justify_length = int(input("Enter length: "))
 
 #Calculate and append number of spaces to reach the total length.
 
