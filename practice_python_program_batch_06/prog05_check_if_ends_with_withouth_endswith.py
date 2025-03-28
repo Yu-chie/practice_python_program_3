701844631338793a1d56daed73a7a0b3ef4cb3eb
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-28 18:35:35 +0800

    Prog05: Pseudocode Added

---
+++ practice_python_program_batch_06/prog05_check_if_ends_with_withouth_endswith.py
@@ -0,0 +1,10 @@
+#Prog05. endswith() check if the string end part matches the function parameter. 
+#Create a program that do the same functionality without using endswith() function.
+
+#Ask User for text and suffix t check
+
+#Get the length of the suffix
+
+#Compare last characters of text with suffix
+
+#Print the result
