be06c283178815a3b241152a4ea01ce6cd92e7ec
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-28 18:36:05 +0800

    Prog05: Ask user for text

--- practice_python_program_batch_06/prog05_check_if_ends_with_withouth_endswith.py
+++ practice_python_program_batch_06/prog05_check_if_ends_with_withouth_endswith.py
@@ -1,9 +1,12 @@
 #Prog05. endswith() check if the string end part matches the function parameter. 
 #Create a program that do the same functionality without using endswith() function.
 
-#Ask User for text and suffix t check
+#Ask User for text and suffix to check
+text = input("Enter a text: ")
+suffix = input("Enter a suffix to check: ")
 
 #Get the length of the suffix
+
 
 #Compare last characters of text with suffix
 
