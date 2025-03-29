d5700c9b01d0cf07b20e0f7aab0b7cfbd4d1eb31
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-29 19:31:51 +0800

    Prog09: Pseudocode Added

---
+++ practice_python_program_batch_06/prog09_capitalize_first_letter_without_using_capitalize_function.py
@@ -0,0 +1,8 @@
+#Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. 
+#Create a program that do the same functionality without using capitalize() function.
+
+#Ask User for text
+
+#Capitalize first letter and convert remaining to ascii_lowercase
+
+#Print capitalized text
