2b59261109e1fcb8dfeb39f1b020ff57f90cb071
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-29 19:46:43 +0800

    Prog10: Pseudocode Added

---
+++ practice_python_program_batch_06/prog10_convert_to_titlecase_without_using_title.py
@@ -0,0 +1,8 @@
+#Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. 
+#Create a program that do the same functionality without using title() function.
+
+#Ask User for text
+
+#Convert each word to title case
+
+#Print title cased text
