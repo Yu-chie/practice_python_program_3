f792e5d0f697354bc18bf173ec5db17530fbdbce
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-29 19:52:57 +0800

    Prog10: Convert each word to title case

--- practice_python_program_batch_06/prog10_convert_to_titlecase_without_using_title.py
+++ practice_python_program_batch_06/prog10_convert_to_titlecase_without_using_title.py
@@ -5,5 +5,12 @@
 text = input("Enter a text: ")
 
 #Convert each word to title case
+words = text.split()
+title_cased_words = []
+for word in words:
+    if word:
+        title_word = chr(ord(word[0]) - 32) if "a" <= word[0] <= "z" else word[0]
+        title_word += "".join(chr(ord(c) + 32) if "A" <= c <= "Z" else c for c in word[1:])
+        title_cased_words.append(title_word)
 
 #Print title cased text
