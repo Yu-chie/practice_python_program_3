67f07e47b41aef78f6ea893dc78a4951c4c33698
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-30 10:20:53 +0800

    Prog05: Pseudocode Added

---
+++ practice_python_program_batch_07/prog05_check_starts_with_manually.py
@@ -0,0 +1,8 @@
+#Prog05. startswith() check if the string beginning part matches the function parameter.
+#Create a program that do the same functionality without using startswith() function.
+
+#Ask User for text and prefix
+
+#Check if text starts with prefix
+
+#print result
