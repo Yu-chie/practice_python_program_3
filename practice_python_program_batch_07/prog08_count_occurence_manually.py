4133c1fc6d5d55ef343aad10a7738d0f2511e8de
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-30 15:43:22 +0800

    Prog08: Ask user for text and substring

--- practice_python_program_batch_07/prog08_count_occurence_manually.py
+++ practice_python_program_batch_07/prog08_count_occurence_manually.py
@@ -2,7 +2,8 @@
 #Create a program that do the same functionality without using count() function.
 
 #Ask user for text and substring
-
+text = input("Enter a text: ")
+substring = input("Enter substring to count: ")
 
 #Manual count
 
