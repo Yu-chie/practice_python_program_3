576568cfd3d730e64a32d26e1903955984950655
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-30 15:52:00 +0800

    Prog10: Pseudocode Added

---
+++ practice_python_program_batch_07/prog10_find_last_index_manually.py
@@ -0,0 +1,8 @@
+#Prog10. rindex() return the first location of the function parameter in the string starting from the last character.
+#Create a program that do the same functionality without using rindex() function.
+
+#Ask User for text and substring to find
+
+#Find index manually
+
+#Print result
