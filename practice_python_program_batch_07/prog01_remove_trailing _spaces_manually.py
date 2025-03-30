9ba2ffee25dd813e45b73a3269b1bd01c5b2a0e0
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-30 09:59:47 +0800

    prog01: Extract string up to the last non space character

--- practice_python_program_batch_07/prog01_remove_trailing _spaces_manually.py
+++ practice_python_program_batch_07/prog01_remove_trailing _spaces_manually.py
@@ -10,5 +10,6 @@ while last_char_index >= 0 and text[last_char_index] == " ":
     last_char_index -= 1
 
 #Extract string up to the last non space character
+cleaned_text = text[:last_char_index +1]
 
 #Print the cleaned text
