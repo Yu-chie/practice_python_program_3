5acc4945f8279b9d2b64468d19a69d399bc7ab18
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-30 10:11:01 +0800

    Prog03: Pseudocode Added

---
+++ practice_python_program_batch_07/prog03_ convert_to_uppercase_manually.py
@@ -0,0 +1,11 @@
+#Prog03. upper() converts all characters of the string into upper case.
+#Create a program that do the same functionality without using upper() function.
+
+#Ask User for text
+
+#Convert to uppercase Manually
+## Check if lowercase
+##Convert to uppercase
+
+#Print uppercase text
+
