b9857aa2a90daef9c41eb631ed6ef3d1779dc048
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-30 09:56:49 +0800

    Prog01: Pseudocode Added

---
+++ practice_python_program_batch_07/prog01_remove_trailing _spaces_manually.py
@@ -0,0 +1,10 @@
+#Prog01. rstrip() remove the space characters at the end of the string. 
+#Create a program that do the same functionality without using rstrip() function.
+
+#Ask User for text
+
+#Find the first non-space character
+
+# Extract string up to the last non space character
+
+#Print the cleaned text
