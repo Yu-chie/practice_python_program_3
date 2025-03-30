5e33d31bf2f06773f6de279be6c91fa1a9115c9d
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-30 15:42:53 +0800

    Prog08: Pseudocode Added

---
+++ practice_python_program_batch_07/prog08_count_occurence_manually.py
@@ -0,0 +1,9 @@
+#Prog08. count() return how many time the function parameter appear in the string. 
+#Create a program that do the same functionality without using count() function.
+
+#Ask user for text and substring
+
+
+#Manual count
+
+#Print result
