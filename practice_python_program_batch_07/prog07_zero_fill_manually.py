6bd4a243fba9933de1614f8fcf30eeff1e356050
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-30 15:39:23 +0800

    Prog07: Rename variable to num and Calculate zeros needed

--- practice_python_program_batch_07/prog07_zero_fill_manually.py
+++ practice_python_program_batch_07/prog07_zero_fill_manually.py
@@ -1,11 +1,12 @@
 #Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter.
 #Create a program that do the same functionality without using zfill() function.
 
-#Ask User for Number ang length
-text = input("Enter a number: ")
+#Ask User for Number and length
+num = input("Enter a number: ")
 length = int(input("Enter total length: "))
 
 #Calculate zeros needed 
+zeros_needed = max(0, length - len(num))
 
 #Add zeros
 
