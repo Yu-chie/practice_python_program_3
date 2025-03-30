dfb4c3658cb70c2f92994f827f9d0340f4cff3b1
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-30 09:57:19 +0800

    Prog01: Ask User for text

--- practice_python_program_batch_07/prog01_remove_trailing _spaces_manually.py
+++ practice_python_program_batch_07/prog01_remove_trailing _spaces_manually.py
@@ -2,9 +2,10 @@
 #Create a program that do the same functionality without using rstrip() function.
 
 #Ask User for text
+text = input("Enter a text: ")
 
 #Find the first non-space character
 
-# Extract string up to the last non space character
+#Extract string up to the last non space character
 
 #Print the cleaned text
