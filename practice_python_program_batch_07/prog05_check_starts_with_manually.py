92aed47526c5bbe26c22c3e760d3b2a8fe28b680
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-30 10:23:13 +0800

    Prog05: Ask User for text and prefix

--- practice_python_program_batch_07/prog05_check_starts_with_manually.py
+++ practice_python_program_batch_07/prog05_check_starts_with_manually.py
@@ -2,6 +2,8 @@
 #Create a program that do the same functionality without using startswith() function.
 
 #Ask User for text and prefix
+text = input("Enter a text: ")
+prefix = input ("Enter prefix to check: ")
 
 #Check if text starts with prefix
 
