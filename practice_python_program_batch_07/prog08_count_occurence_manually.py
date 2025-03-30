388bb94a593f72453fd1752da36ce62027e60a4e
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-30 15:45:03 +0800

    Prog08: Manual count

--- practice_python_program_batch_07/prog08_count_occurence_manually.py
+++ practice_python_program_batch_07/prog08_count_occurence_manually.py
@@ -6,5 +6,12 @@ text = input("Enter a text: ")
 substring = input("Enter substring to count: ")
 
 #Manual count
+count = 0
+index = 0
+
+while index <= len(text) - len (substring):
+    if text[index:index+len(substring)] == substring:
+        count += 1
+    index += 1
 
 #Print result
