a0cb5b9027ddc1dedd395d529f8f36866a10294e
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-30 15:48:52 +0800

    Prog09: Find index manually

--- practice_python_program_batch_07/prog09_find_first_index_manually.py
+++ practice_python_program_batch_07/prog09_find_first_index_manually.py
@@ -6,5 +6,10 @@ text = input("Enter a text: ")
 substring = input("Enter substring to find: ")
 
 #Find index manually
-
+index = -1
+for i in range(len(text)- len(substring) + 1):
+    if text[i:i+len(substring)] == substring:
+        index = 1
+        break
+    
 #Print result
