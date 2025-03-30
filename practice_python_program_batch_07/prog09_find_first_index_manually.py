f3f7d5036c1539bfbb3fca484c304701596c9d45
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-30 15:46:43 +0800

    Prog09: Pseudocode Added

---
+++ practice_python_program_batch_07/prog09_find_first_index_manually.py
@@ -0,0 +1,8 @@
+#Prog09. index() return the first location of the function parameter in the string.
+#Create a program that do the same functionality without using index() function.
+
+#Ask User for text and substring to find
+
+#Find index manually
+
+#Print result
