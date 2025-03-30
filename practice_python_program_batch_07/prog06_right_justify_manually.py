f2e55a6c12b7126cd0a918a54146d3a52dccd5b7
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-30 15:31:54 +0800

    Prog06: Ask User for text and spaces to justify

--- practice_python_program_batch_07/prog06_right_justify_manually.py
+++ practice_python_program_batch_07/prog06_right_justify_manually.py
@@ -1,7 +1,10 @@
 #Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter.
 #Create a program that do the same functionality without using rjust() function.
 
-#Ask User for text
+#Ask User for text and spaces to justify
+text = input("Enter a text: ")
+length = int(input("Enter total width: "))
+
 
 #Calculate spaces needed
 
