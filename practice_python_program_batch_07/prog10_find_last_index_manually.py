47a8085307794f4ddb09535c636e3dd1aab8ca8d
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-30 15:52:17 +0800

    Prog10: Ask User for text and substring to find

--- practice_python_program_batch_07/prog10_find_last_index_manually.py
+++ practice_python_program_batch_07/prog10_find_last_index_manually.py
@@ -2,6 +2,8 @@
 #Create a program that do the same functionality without using rindex() function.
 
 #Ask User for text and substring to find
+text = input("Enter a text: ")
+substring = input("Enter substring to find: ")
 
 #Find index manually
 
