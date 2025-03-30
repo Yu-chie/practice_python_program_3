8f9688ef35b75164d2d65143c906a41812252af0
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-30 15:31:15 +0800

    Prog06: Pseudocode Added

---
+++ practice_python_program_batch_07/prog06_right_justify_manually.py
@@ -0,0 +1,10 @@
+#Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter.
+#Create a program that do the same functionality without using rjust() function.
+
+#Ask User for text
+
+#Calculate spaces needed
+
+#Create right-justified text 
+
+#Print result
