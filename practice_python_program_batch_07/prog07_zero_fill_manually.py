465d5c5526ec80721fb935b915d7eb69bc443a4a
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-30 15:38:05 +0800

    Prog07: Ask User for Number ang length

--- practice_python_program_batch_07/prog07_zero_fill_manually.py
+++ practice_python_program_batch_07/prog07_zero_fill_manually.py
@@ -1,9 +1,9 @@
 #Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter.
 #Create a program that do the same functionality without using zfill() function.
 
-#Ask User 
-
 #Ask User for Number ang length
+text = input("Enter a number: ")
+length = int(input("Enter total length: "))
 
 #Calculate zeros needed 
 
