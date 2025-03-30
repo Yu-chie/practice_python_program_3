85a6e33c46b25b599e700f431cea490d5eb4c175
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-30 10:16:11 +0800

    Prog04: Pseudocode Added

---
+++ practice_python_program_batch_07/prog04_check_all_lowercase_manually.py
@@ -0,0 +1,8 @@
+#Prog04. islower() check if all characters of the string is on lower case. 
+#Create a program that do the same functionality without using islower() function.
+
+#Ask user for Text
+
+#Check if letters are lowercased manually
+
+#Print result
