dc1204e3b5db0656d8f46cc0a96eba651a8009a8
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-30 10:03:45 +0800

    Prog02: Pseudocode Added

---
+++ practice_python_program_batch_07/prog02_remove_suffix_manually.py
@@ -0,0 +1,8 @@
+#Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter.
+#Create a program that do the same functionality without using removesuffix() function.
+
+#Ask User for text and suffix to remove
+
+#Check if text ends with the suffix
+
+#Print result
