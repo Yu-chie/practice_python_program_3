765b877a85f06d516d0cebb112bd29ca5eb94e22
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-30 15:37:34 +0800

    Prog07: Pseudocode Added

---
+++ practice_python_program_batch_07/prog07_zero_fill_manually.py
@@ -0,0 +1,12 @@
+#Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter.
+#Create a program that do the same functionality without using zfill() function.
+
+#Ask User 
+
+#Ask User for Number ang length
+
+#Calculate zeros needed 
+
+#Add zeros
+
+#Print result
